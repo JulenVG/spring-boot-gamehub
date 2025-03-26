9407fef978b3bfb6676a9dee7b3ea9a63a7ac496
julen <julen817@gmail.com>
2025-03-26 11:38:06 +0100

    Added Category modify error tests

--- src/test/java/com/ccsw/tutorial/category/CategoryIT.java
+++ src/test/java/com/ccsw/tutorial/category/CategoryIT.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 
@@ -61,7 +62,7 @@ public class CategoryIT {
         assertNotNull(categorySearch);
         assertEquals(NEW_CATEGORY_NAME, categorySearch.getName());
     }
-    
+
     public static final Long MODIFY_CATEGORY_ID = 3L;
 
     @Test
@@ -79,5 +80,16 @@ public class CategoryIT {
         CategoryDto categorySearch = response.getBody().stream().filter(item -> item.getId().equals(MODIFY_CATEGORY_ID)).findFirst().orElse(null);
         assertNotNull(categorySearch);
         assertEquals(NEW_CATEGORY_NAME, categorySearch.getName());
+    }
+
+    @Test
+    public void modifyWithNotExistIdShouldInternalError() {
+
+        CategoryDto dto = new CategoryDto();
+        dto.setName(NEW_CATEGORY_NAME);
+
+        ResponseEntity<?> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH + "/" + NEW_CATEGORY_ID, HttpMethod.PUT, new HttpEntity<>(dto), Void.class);
+        
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
     }
 }
